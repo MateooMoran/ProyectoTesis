c325a1201a942c8e18631fa964a83aba3735eb2b
24DavidM <151885025+24DavidM@users.noreply.github.com>
2025-06-16 13:51:58 -0500

    Ojo V5

--- Frontend-Poli/src/pages/Reset.jsx
+++ Frontend-Poli/src/pages/Reset.jsx
@@ -1,4 +1,4 @@
-import logoReset from '../assets/reset.webp'; 
+import logoReset from '../assets/reset.webp';
 import { ToastContainer, toast } from 'react-toastify';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -12,6 +12,7 @@ const Reset = () => {
     const { fetchDataBackend } = useFetch();
     const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
 
     const changePassword = async (data) => {
         const { password, confirmPassword } = data;
@@ -45,7 +46,7 @@ const Reset = () => {
             }, 3000);
         } catch (error) {
             console.log(error);
-            
+
         }
     };
 
@@ -94,6 +95,23 @@ const Reset = () => {
                             {...register("password", { required: "La contraseña es obligatoria" })}
                         />
                         {errors.password && <p className="text-red-700 text-sm mt-1">{errors.password.message}</p>}
+                        {/* Botón para mostrar/ocultar la contraseña */}
+                        <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute top-2 right-3 text-gray-500 hover:text-gray-700"
+                        >
+                            {/* Icono que cambia según el estado de la contraseña */}
+                            {showPassword ? (
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.875 18.825A9.956 9.956 0 0112 19c-4.418 0-8.165-2.928-9.53-7a10.005 10.005 0 0119.06 0 9.956 9.956 0 01-1.845 3.35M9.9 14.32a3 3 0 114.2-4.2m.5 3.5l3.8 3.8m-3.8-3.8L5.5 5.5" />
+                                </svg>
+                            ) : (
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0zm-9.95 0a9.96 9.96 0 0119.9 0m-19.9 0a9.96 9.96 0 0119.9 0M3 3l18 18" />
+                                </svg>
+                            )}
+                        </button>
                     </div>
 
                     {/* Campo de confirmar contraseña */}
@@ -108,6 +126,22 @@ const Reset = () => {
                             {...register("confirmPassword", { required: "La confirmación es obligatoria" })}
                         />
                         {errors.confirmPassword && <p className="text-red-700 text-sm mt-1">{errors.confirmPassword.message}</p>}
+                        <button
+                            type="button"
+                            onClick={() => setShowPassword(!showPassword)}
+                            className="absolute top-2 right-3 text-gray-500 hover:text-gray-700"
+                        >
+                            {/* Icono que cambia según el estado de la contraseña */}
+                            {showPassword ? (
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.875 18.825A9.956 9.956 0 0112 19c-4.418 0-8.165-2.928-9.53-7a10.005 10.005 0 0119.06 0 9.956 9.956 0 01-1.845 3.35M9.9 14.32a3 3 0 114.2-4.2m.5 3.5l3.8 3.8m-3.8-3.8L5.5 5.5" />
+                                </svg>
+                            ) : (
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0zm-9.95 0a9.96 9.96 0 0119.9 0m-19.9 0a9.96 9.96 0 0119.9 0M3 3l18 18" />
+                                </svg>
+                            )}
+                        </button>
                     </div>
 
                     {/* Botón para enviar */}
