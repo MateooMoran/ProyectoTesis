7fdd81a904d1b4f457d15b15c54ee7279a0bfd57
24DavidM <151885025+24DavidM@users.noreply.github.com>
2025-07-05 14:00:36 -0500

    dsd

--- Backend-Poli/src/controllers/estudiante_controller.js
+++ Backend-Poli/src/controllers/estudiante_controller.js
@@ -109,7 +109,7 @@ const crearNuevoPassword = async (req, res) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body
-
+    console.log(email,password)
     if (Object.values(req.body).includes("")) return res.status(400).json({ msg: "Lo sentimos debes llenar todo los campos" });
 
     const estudianteBDD = await Estudiante.findOne({ email }).select("-status -__v -updatedAt -createdAt")
@@ -123,6 +123,7 @@ const login = async (req, res) => {
 
     const { nombre, apellido, direccion, telefono, _id, rol } = estudianteBDD
     const token = createTokenJWT(estudianteBDD._id, estudianteBDD.rol)
+    console.log(token)
     res.status(200).json({
         token,
         rol,
