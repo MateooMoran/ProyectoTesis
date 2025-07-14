756302ef558608bf96aa1537b966ae306a1140bf
24DavidM <151885025+24DavidM@users.noreply.github.com>
2025-07-13 22:43:51 -0500

    Update Dashboard and Add Slidebar

--- Frontend-Poli/src/layout/Dashboard.jsx
+++ Frontend-Poli/src/layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "./Sidebar";
+import { Sidebar } from "../pages/Sidebar";
 
 export const Dashboard = () => {
   return (
@@ -6,6 +6,8 @@ export const Dashboard = () => {
       <Sidebar></Sidebar>
       <main className="flex-1 p-6">
         <h1 className="text-4xl font-bold text-blue-950">Bienvenido al Panel</h1>
+        <h2>Complete</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, repellat. Accusamus consectetur sapiente labore mollitia dignissimos, illo dolorum repudiandae, amet iure quis sequi voluptatum tempora eos fuga. Cupiditate, eveniet quaerat.</p>
       </main>
     </div>
   );
