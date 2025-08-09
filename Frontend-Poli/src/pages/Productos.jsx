8e335ddc7d0fc83e07cd06b4d2e686667953aac2
MateooMoran <brandon.moran@epn.edu.ec>
2025-08-08 19:26:07 -0500

    nofuncax3

--- Frontend-Poli/src/pages/Productos.jsx
+++ Frontend-Poli/src/pages/Productos.jsx
@@ -250,7 +250,7 @@ const Productos = () => {
               >
                 {productos.map((producto) => (
                   <SwiperSlide key={producto._id}>
-                    <Link to={`/estudiante/productos/${producto._id}`} className="block">
+                    <Link to={`/productos/${producto._id}`} className="block">
                       <div className="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-lg transition-shadow duration-300 p-4">
                         <div className="relative">
                           <img
